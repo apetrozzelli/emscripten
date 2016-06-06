e9649d4b8ed04a3433bbce04609c624af8c06abc
Alessandro Petrozzelli <a.petrozzelli@iontrading.com>
2016-06-06 15:27:28 +0200

    checkout dependencies

--- build-with-packer.sh
+++ build-with-packer.sh
@@ -1,4 +1,8 @@
-cd ./dependencies/ubuntu
+#!/usr/bin/env bash
+
+git submodule init
+git submodule update
+cd dependencies/ubuntu
 cat ../../setup-emscripten.sh >> ./custom-script.sh
 packer build -only=virtualbox-iso -var-file=../../ubuntu1404-emscripten.json ubuntu.json
 git checkout custom-script.sh
