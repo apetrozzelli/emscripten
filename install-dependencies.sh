525eebf94fe87411922b0f8ee9d450738298d437
Alessandro Petrozzelli <apetrozzelli@gmail.com>
2017-01-02 19:30:03 +0100

    moved to packer 0.12.1

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,2 +1,2 @@
-wget --no-check-certificate https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip && unzip -d packer packer_0.10.1_linux_amd64.zip
+wget --no-check-certificate https://releases.hashicorp.com/packer/0.12.1/packer_0.12.1_linux_amd64.zip && unzip -d packer packer_0.12.1_linux_amd64.zip
 
