08cfd53b624de4ba27f547b64c22bd4715ec865c
Alessandro Petrozzelli <apetrozzelli@gmail.com>
2017-03-03 23:25:51 +0100

    trying again with packer (0.12.3)

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,2 +1,2 @@
-wget --no-check-certificate https://releases.hashicorp.com/packer/0.12.2/packer_0.12.2_linux_amd64.zip && rm -rf packer && unzip -d packer packer_0.12.2_linux_amd64.zip
+wget --no-check-certificate https://releases.hashicorp.com/packer/0.12.3/packer_0.12.3_linux_amd64.zip && rm -rf packer && unzip -d packer packer_0.12.3_linux_amd64.zip
 
